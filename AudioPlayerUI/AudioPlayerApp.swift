1a05f36ec894eb57a03c7f821d1ae3019e5b2c21
William.Weng <William.Weng@iit.com.hk>
2026-07-24 09:39:08 +0800

    - 隱藏系統音量HUD

--- AudioPlayerUI/AudioPlayerApp.swift
+++ AudioPlayerUI/AudioPlayerApp.swift
@@ -11,9 +11,26 @@ import SwiftUI
 struct AudioPlayerApp: App {
     
     var body: some Scene {
+        
         WindowGroup {
-            ContentView()
-                .preferredColorScheme(.light)
+            
+            ZStack {
+                ContentView()
+                hiddenVolumeView
+            }
+            .preferredColorScheme(.light)
         }
+    }
+}
+
+// MARK: - 私有屬性
+private extension AudioPlayerApp {
+    
+    /// 隱藏系統音量HUD用
+    var hiddenVolumeView: some View {
+        
+        HiddenVolumeView()
+            .frame(width: 1, height: 1)
+            .opacity(0.01)
     }
 }
---
+++ AudioPlayerUI/View/HiddenVolumeView.swift
@@ -0,0 +1,19 @@
+//
+//  HiddenVolumeView.swift
+//  AudioPlayerUI
+//
+//  Created by William.Weng on 2026/7/24.
+//
+
+import SwiftUI
+import MediaPlayer
+
+// MARK: - 輔助元件：阻擋系統自帶 HUD 專用
+struct HiddenVolumeView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MPVolumeView {
+        .init(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MPVolumeView, context: Context) {}
+}
